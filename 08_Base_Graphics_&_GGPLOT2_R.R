6ee887869b2b1da384e1c339fb564e30eb35785f
Monica G. Bidart <bidartmgb@hotmail.com>
2020-09-23 11:16:25 -0300

    Script con ejemplos de Gráficas con Base & con GGPLOT2

---
+++ 08_Base_Graphics_&_GGPLOT2_R.R
@@ -0,0 +1,66 @@
+# T2V4 - graficar en R
+
+# Objetivo: diferenciar formas de graficar en r
+# ------------------------------------------------
+# En este ejercicio vamos a:
+# 1. graficar con base graphics
+# 2. graficar con ggplot2
+
+# DATO VIP: https://www.r-graph-gallery.com
+# datos de la primera parte
+year <- c('2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018')
+disney <- c(11, 13, 11, 8, 12, 11, 12, 8, 10)
+
+# graficando con codigo
+plot(x = year,
+     y = disney)
+
+# editando la grafica
+plot(x = year,
+    y = disney,
+    main = 'disney',
+    xlab = '',
+    ylab = '',
+    col = 'cornflowerblue', 
+    pch = 16,
+    panel.first = grid() )
+--------------- ###   EXPERIMENTANDO   ### ---------------
+barplot()
+hist()
+
+# Version 1.0 ya se tiene un vector con los valores a mostrar en %
+label <- paste(disney,"%",sep = " ")
+pie(disney,
+    labels = label,
+    clockwise = TRUE,
+    main = "Grafica de No Se Que")
+
+# Version 2.0
+color <- c("green","blue","lightgray","purple","darkolivegreen","orange","yellow","black","pink")
+pie(disney,
+    labels = label,
+    clockwise = TRUE,
+    main = "Grafica de No Se Que 2",
+    col = color )
+legend("topright",c("11","13","11","8","12","11","12","8","10"),cex = 0.8,fill = color)
+
+--------------- ###   FIN EXPERIMENTANDO   ### ---------------
+# Graficas con ggplot2
+# install.package(ggplot2) # para instalar ggplot2
+library(ggplot2)
+
+# hacer dataframe
+peliculas <- data.frame(year, 
+                        disney)
+
+# graficar utilizando ggplot
+ggplot(data = peliculas,
+       mapping = aes(x = year,
+                     y = disney)) +
+  geom_point() +
+  labs(title = 'Disney')
+  
+
+
+
+# fin
